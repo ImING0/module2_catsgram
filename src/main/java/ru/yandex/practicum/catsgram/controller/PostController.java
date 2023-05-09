241e6f11b85c0f283334ffb298abc252e03e614b
George Polyansky <83121990+ImING0@users.noreply.github.com>
2023-05-09 17:28:28 +0300

    feat
    добавить пост сервис

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,32 +1,31 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import com.google.gson.Gson;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.model.Post;
-import java.util.ArrayList;
+import ru.yandex.practicum.catsgram.service.PostService;
+
 import java.util.List;
 
 @RestController
 public class PostController {
+    private final PostService postService;
 
-    private final List<Post> posts = new ArrayList<>();
-
-    private final static Logger logger = LoggerFactory.getLogger(PostController.class);
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/posts")
     public List<Post> findAll() {
-        logger.debug("Текущее количество постов: {}",  posts.size());
-        return posts;
+        return postService.findAll();
     }
 
     @PostMapping(value = "/post")
-    public void create(@RequestBody Post post) {
-        logger.debug("Объект: {}", new Gson().toJson(post));
-        posts.add(post);
+    public Post create(@RequestBody Post post) {
+        return postService.create(post);
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,63 +1,36 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import com.google.gson.Gson;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exciptions.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exciptions.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.util.*;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
-    private final HashMap<String, User> users = new HashMap();
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        if (user.getEmail()
-                .isEmpty() || user.getEmail()
-                .isBlank() || user.getEmail() == null) {
-            try {
-                throw  new InvalidEmailException("email адрес пуст " + user.getEmail());
-            } catch (InvalidEmailException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        if (users.containsKey(user.getEmail())) try {
-            throw new UserAlreadyExistException("Пользователь с таким email уже существует" + user.getEmail());
-        } catch (UserAlreadyExistException e) {
-            System.out.println(e.getMessage());
-        }
-
-        log.debug("Объект: {}", new Gson().toJson(user));
-        users.put(user.getEmail(), user);
-        return user;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
-    public List<User> getUsers() {
-        log.debug("Кол-во пользователей в текущий момент: {}", users.size());
-        return new ArrayList<>(users.values());
+    public Collection<User> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) throws UserAlreadyExistException, InvalidEmailException {
+        return userService.createUser(user);
     }
 
     @PutMapping
-    public User updateOrCreateUser(@RequestBody User user) {
-        if (user.getEmail()
-                .isEmpty() || user.getEmail()
-                .isBlank() || user.getEmail() == null) {
-            try {
-                throw  new InvalidEmailException("email адрес пуст " + user.getEmail());
-            } catch (InvalidEmailException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        users.put(user.getEmail(), user);
-        return user;
+    public User updateUser(@RequestBody User user) throws InvalidEmailException {
+        return userService.updateUser(user);
     }
 }
