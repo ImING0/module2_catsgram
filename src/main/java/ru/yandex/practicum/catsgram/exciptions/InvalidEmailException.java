b733403904cd4da8ae2b1e765df5b4bdcbde32ce
George Polyansky <83121990+ImING0@users.noreply.github.com>
2023-05-13 18:23:50 +0300

    feat
    реализовать перехват исключений

--- src/main/java/ru/yandex/practicum/catsgram/exciptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.catsgram.exciptions;
-
-public class InvalidEmailException extends Exception{
-    public InvalidEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/catsgram/exciptions/PostNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.catsgram.exciptions;
-
-public class PostNotFoundException extends RuntimeException{
-
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
